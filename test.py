bd694f19d5ea61acf087bfb772e8a7c58c7a1cd3
C-A de Salaberry <cadesalaberry@yahoo.com>
2013-03-02 21:33:19 -0500

    Add: max, min and noarg.

--- test.py
+++ test.py
@@ -15,16 +15,20 @@ class MiniDCTestCase(unittest.TestCase):
 		for i in range(100):
 			for j in range(100):
 				assert minidc([i,j,'+']) == i + j, 'Logic:addition test did not pass.'
-				assert minidc([i,j,'-']) == i - j, 'Logic:substraction test did not pass.'
+				assert minidc([i,j,'-']) == i - j, 'Logic:subtraction test did not pass.'
 				assert minidc([i,j,'*']) == i * j, 'Logic:multiplication test did not pass.'
 				assert minidc([i,j,'/']) == i / j, 'Logic:division test did not pass.'
+
+		#The program should refuse numbers if they are too big
+		assert minidc([-sys.maxint - 1,1,'+']) == 'Out of Bound.', 'Logic:minimum test did not pass'
+		assert minidc([sys.maxint + 1,1,'-']) == 'Out of Bound.', 'Logic:maximum test did not pass'
 
 	def testSyntax():
 
 		#Ideally, there should exist a checkSyntax([]) method returning false on error
 		print "Testing correctness of Syntax:"
 
-		assert True == checkSyntax([1,2,'+']), 'Syntax:reverse-polish test did not pass'
+		assert checkSyntax([1,2,'+']), 'Syntax:reverse-polish test did not pass'
 		assert False == checkSyntax([3,'+',4]), 'Syntax:infix test did not pass'
 		assert False == checkSyntax(['+',3,5]), 'Syntax:polish test did not pass'
 
@@ -33,12 +37,15 @@ class MiniDCTestCase(unittest.TestCase):
 
 
 		#Checks the different types of valid syntax
-		assert True == checkSyntax([1,3,'+',2,'-']), 'Syntax:lefty-operation test did not pass'
-		assert True == checkSyntax([1,3,2,'-','+']), 'Syntax:righty-operation test did not pass'
-		assert True == checkSyntax([1,3,'+',2,4,'-','*']), 'Syntax:pyramid-operation test did not pass'
+		assert checkSyntax([1,3,'+',2,'-']), 'Syntax:lefty-operation test did not pass'
+		assert checkSyntax([1,3,2,'-','+']), 'Syntax:righty-operation test did not pass'
+		assert checkSyntax([1,3,'+',2,4,'-','*']), 'Syntax:pyramid-operation test did not pass'
 
 
 	def testParameters():
+
+		print "Testing implementation of Parameters:"
+		assert minidc([]) == 'Wrong usage. Please refer to README file for available commands.', 'Parameters:no-input test did not pass'
 
 # Simple way to make a test suite
 def makeMiniDCTestSuite():
