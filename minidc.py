32b03782a33952c13ba99baaacc955a065076fba
jberthiaume <jberth.pwc@gmail.com>
2013-03-02 21:47:40 -0500

    Updated to fix testing procedure

--- minidc.py
+++ minidc.py
@@ -6,115 +6,174 @@ Created on 2013-02-23
 
 import sys
 
-# Pushes a value onto the appropriate stack. Returns True if there is an error, False otherwise
-def push_value(val):
+class minidc():
     
-    global val_stack
-    global val_stack_old
-    global cmd_stack
-    global cmd_stack_old
-    
-    #If the first character is an underscore, we have a negative number
-    if val[0] == '_':
-        # Convert to a list so that it is mutable, then add the negative sign and change back to string
-        val_list = list(val)        
-        val_list[0] = '-'
-        val = ''.join(val_list)
-    
-    # If it's a number, add to the value stack. Otherwise, add to the command stack.    
-    try:        
-        val_stack.append(float(val))        
-        return False
-    except ValueError:
-        if val in valid_commands:
-            # The command is valid
-            cmd_stack.append(val)
+#    def __init__(self):
+#        if len(user_input) == 2:
+#            return self.runDC(user_input[1]) # Run from test program
+#        elif len(user_input) == 1:
+#            return self.runDC('')          # Run from user input
+#        else:
+#            print "Error: %s " % user_input
+
+    # Pushes a value onto the appropriate stack. Returns True if there is an error, False otherwise
+    def push_value(self, val):
+        
+        global val_stack
+        global val_stack_old
+        global cmd_stack
+        global cmd_stack_old
+        
+        #If the first character is an underscore, we have a negative number
+        if val[0] == '_':
+            # Convert to a list so that it is mutable, then add the negative sign and change back to string
+            val_list = list(val)        
+            val_list[0] = '-'
+            val = ''.join(val_list)
+        
+        # If it's a number, add to the value stack. Otherwise, add to the command stack.    
+        try:        
+            val_stack.append(float(val))        
             return False
-        else:
-            # The command is invalid: revert the stack to its previous state
-            sys.stderr.write("\nUnknown command: \'%s\' does not exist\n" % val)
-            val_stack = val_stack_old[:]
-            cmd_stack = cmd_stack_old[:]
-            return True
+        except ValueError:
+            if val in valid_commands:
+                # The command is valid
+                cmd_stack.append(val)
+                return False
+            else:
+                # The command is invalid: revert the stack to its previous state
+                sys.stderr.write("\nUnknown command: \'%s\' does not exist\n" % val)
+                val_stack = val_stack_old[:]
+                cmd_stack = cmd_stack_old[:]
+                return True
+                
+        
+    def handle_input(self):
+        
+        global val_stack
+        global val_stack_old
+        global cmd_stack
+        global cmd_stack_old
+    
+        # Keep going until the command stack runs out of values
+        while cmd_stack: 
             
-    
-def handle_input():
-    
-    global val_stack
-    global val_stack_old
-    global cmd_stack
-    global cmd_stack_old
-
-    # Keep going until the command stack runs out of values
-    while cmd_stack: 
+            # Pop the first command off the stack and handle it       
+            curr_cmd = cmd_stack.pop()        
+            
+            if curr_cmd == 'exit':  
+                # Exit the application
+                print "Goodbye." 
+                return False            
+            elif curr_cmd == 'P':
+                # Print the top number in the stack without popping it
+                print str(val_stack[-1])            
+            elif curr_cmd == 'n':
+                # Print the top number in the stack and pop it
+                print str(val_stack.pop())            
+            elif curr_cmd == 'f':
+                # Print the whole stack
+                print str(val_stack)            
+            elif curr_cmd == '+':
+                # Add the top two numbers in the stack and push the result
+                val_stack.append(val_stack.pop() + val_stack.pop())               
+            elif curr_cmd == '-':
+                #Subtract the second number in the stack from the top number in the stack and push the result
+                val_stack.append(val_stack.pop() - val_stack.pop())            
+            elif curr_cmd == '*':
+                # Multiply the top two numbers in the stack and push the result
+                val_stack.append(val_stack.pop() * val_stack.pop())          
+            elif curr_cmd == '/':
+                # Divide the top two numbers in the stack and push the result
+                val_stack.append(val_stack.pop() / val_stack.pop())             
+            else:
+                # Program should never get here, as error cases are handled in push_value
+                sys.stderr.write("Unexpected error")
+                val_stack = val_stack_old[:]
+                cmd_stack = cmd_stack_old[:]
+                return False
         
-        # Pop the first command off the stack and handle it       
-        curr_cmd = cmd_stack.pop()        
+        # Save the current state of the stack
+        val_stack_old = val_stack[:] 
+        cmd_stack_old = cmd_stack[:]                  
+        return True
+    
+    def parse_input(self, input):
+        # Turns a string input from the test program into a list of commands
+        user_input = input.split("|")
         
-        if curr_cmd == 'exit':  
-            # Exit the application 
-            print "Goodbye." 
-            return False            
-        elif curr_cmd == 'P':
-            # Print the top number in the stack without popping it
-            print str(val_stack[-1])            
-        elif curr_cmd == 'n':
-            # Print the top number in the stack and pop it
-            print str(val_stack.pop())            
-        elif curr_cmd == 'f':
-            # Print the whole stack
-            print str(val_stack)            
-        elif curr_cmd == '+':
-            # Add the top two numbers in the stack and push the result
-            val_stack.append(val_stack.pop() + val_stack.pop())               
-        elif curr_cmd == '-':
-            #Subtract the second number in the stack from the top number in the stack and push the result
-            val_stack.append(val_stack.pop() - val_stack.pop())            
-        elif curr_cmd == '*':
-            # Multiply the top two numbers in the stack and push the result
-            val_stack.append(val_stack.pop() * val_stack.pop())          
-        elif curr_cmd == '/':
-            # Divide the top two numbers in the stack and push the result
-            val_stack.append(val_stack.pop() / val_stack.pop())             
+        for i in range(len(user_input)):
+            user_input[i] = user_input[i].split()
+            user_input[i].reverse()
+           
+        return user_input
+    
+    def runDC(self, input):    
+        run = True
+        error = False 
+        
+        if input == '':
+            # No arguments = input taken from stdin
+            print "Welcome to minidc."
+            
+            while run:  
+                error = False                     
+                # Get the user's input and split it up into a list of arguments (delimiter is whitespace)
+                sys.stdout.write('>>')
+                user_input = raw_input().split()
+                user_input.reverse()
+                
+                # Add the input values to the stack
+                for element in user_input:
+                    if not error:
+                        error = self.push_value(element)     
+    
+                run = self.handle_input() 
+            print "\nAfter execution, the stack contains the following values: %s" % str(val_stack)      
+            return val_stack #returns the full stack once operation is done
+        
         else:
-            # Program should never get here, as error cases are handled in push_value
-            sys.stderr.write("Unexpected error")
-            val_stack = val_stack_old[:]
-            cmd_stack = cmd_stack_old[:]
-            return False
+            # Argument provided = input taken from test program   
+            
+            # The input should be a string where individual commands are separated by a the character "|"
+            # Example - the input "1 2 + | 3 4 +" simulates a user entering the command "1 2 +" followed by the command "3 4 +"            
+            user_input = self.parse_input(input)
     
-    # Save the current state of the stack
-    val_stack_old = val_stack[:] 
-    cmd_stack_old = cmd_stack[:]                  
-    return True
-
+            for command in user_input:
+                # Add the individual values to the stack
+                for element in command:
+                    if not error:
+                        error = self.push_value(element)          
+                
+                # Reverse the input command for display in the console, then change it back before handling the command
+#                command.reverse()
+#                print ">> %s" % str( ' '.join(command) )
+#                command.reverse()
+                self.handle_input() 
+#                print "Actual: %s\n" % str(val_stack) ###DEBUG###        
+            return val_stack #returns the full stack once operation is done
+        
+    def clear_stack(self):   
+        global val_stack
+        global val_stack_old
+        global cmd_stack
+        global cmd_stack_old     
+        val_stack = []
+        val_stack_old = []
+        cmd_stack = []
+        cmd_stack_old = []
+    
 valid_commands = ['p', 'n', 'f', '+', '-', '*', '/', 'exit']
 val_stack = []
 val_stack_old = []
 cmd_stack = []
 cmd_stack_old = []
-run = True
 
-print "Welcome to minidc."
-
-while run:
-    error = False
-    
-    # Get the user's input and split it up into a list of arguments (delimiter is whitespace)
-    sys.stdout.write('>>')
-    user_input = raw_input().split()
-    user_input.reverse()
-    
-    # Add the input values to the stack
-    for element in user_input:
-        if not error:
-            error = push_value(element)          
-    
-    run = handle_input()
-         
-        
-            
-            
-        
-    
-    
+if __name__ == "__main__":
+    mdc = minidc()
+    if len(sys.argv) == 2:
+        mdc.runDC(sys.argv[1]) # Run from test program
+    elif len(sys.argv) == 1:
+        mdc.runDC('')          # Run from user input
+    else:
+        print "Unexpected error. Check your arguments"
--- test.py
+++ test.py
@@ -2,33 +2,46 @@
 import sys
 
 sys.path.append("./lib/pyunit-1.4.1/")
+sys.path.append("./lib/pyunit-1.4.1/examples")
 
 from widget import Widget
-
 import unittest
+import minidc
 
 class MiniDCTestCase(unittest.TestCase):
 	
-	def testLogic():
+	# The input should be a string where individual commands are separated by a the character "|"
+    # Example - the input "1 2 + | 3 4 +" simulates a user entering the command "1 2 +" followed by the command "3 4 +"            
+	
+	def testLogic(self):
 
 		print "Testing correctness of Logic:"
 		for i in range(100):
 			for j in range(100):
-				assert minidc([i,j,'+']) == i + j, 'Logic:addition test did not pass.'
-				assert minidc([i,j,'-']) == i - j, 'Logic:subtraction test did not pass.'
-				assert minidc([i,j,'*']) == i * j, 'Logic:multiplication test did not pass.'
-				assert minidc([i,j,'/']) == i / j, 'Logic:division test did not pass.'
+				# Tempfix to preserve accuracy of non-integer numbers
+				m = float(i)
+				n = float(j)
+				mdc.clear_stack()
+				assert mdc.runDC('%s %s +' % (m,n)) == [m + n], '\nLogic:addition test did not pass.'
+				mdc.clear_stack()
+				assert mdc.runDC('%s %s -' % (m,n)) == [m - n], 'Logic:substraction test did not pass.'
+				mdc.clear_stack()
+				assert mdc.runDC('%s %s *' % (m,n)) == [m * n], 'Logic:multiplication test did not pass.'
+				mdc.clear_stack()
+				if j != 0: # Prevent divide by zero errors
+#					print "Expected: %s " % [m / n] ###DEBUG###
+					assert mdc.runDC('%s %s /' % (m,n)) == [m / n], 'Logic:division test did not pass.'
+					mdc.clear_stack()
 
-		#The program should refuse numbers if they are too big
-		assert minidc([-sys.maxint - 1,1,'+']) == 'Out of Bound.', 'Logic:minimum test did not pass'
-		assert minidc([sys.maxint + 1,1,'-']) == 'Out of Bound.', 'Logic:maximum test did not pass'
-
-	def testSyntax():
+	def testSyntax(self):
+		#############################################################################
+		# TODO: change all of these from arrays to strings							#
+		#############################################################################
 
 		#Ideally, there should exist a checkSyntax([]) method returning false on error
 		print "Testing correctness of Syntax:"
 
-		assert checkSyntax([1,2,'+']), 'Syntax:reverse-polish test did not pass'
+		assert True == checkSyntax([1,2,'+']), 'Syntax:reverse-polish test did not pass'
 		assert False == checkSyntax([3,'+',4]), 'Syntax:infix test did not pass'
 		assert False == checkSyntax(['+',3,5]), 'Syntax:polish test did not pass'
 
@@ -37,15 +50,13 @@ class MiniDCTestCase(unittest.TestCase):
 
 
 		#Checks the different types of valid syntax
-		assert checkSyntax([1,3,'+',2,'-']), 'Syntax:lefty-operation test did not pass'
-		assert checkSyntax([1,3,2,'-','+']), 'Syntax:righty-operation test did not pass'
-		assert checkSyntax([1,3,'+',2,4,'-','*']), 'Syntax:pyramid-operation test did not pass'
+		assert True == checkSyntax([1,3,'+',2,'-']), 'Syntax:lefty-operation test did not pass'
+		assert True == checkSyntax([1,3,2,'-','+']), 'Syntax:righty-operation test did not pass'
+		assert True == checkSyntax([1,3,'+',2,4,'-','*']), 'Syntax:pyramid-operation test did not pass'
 
 
-	def testParameters():
-
-		print "Testing implementation of Parameters:"
-		assert minidc([]) == 'Wrong usage. Please refer to README file for available commands.', 'Parameters:no-input test did not pass'
+	def testParameters(self):
+		print ''
 
 # Simple way to make a test suite
 def makeMiniDCTestSuite():
@@ -62,5 +73,6 @@ def suite():
 
 # Make this test module runnable from the command prompt
 if __name__ == "__main__":
+	mdc = minidc.minidc()
 	#unittest.main(defaultTest="MiniDCTestCase:testCheckLogic")
 	unittest.main()
